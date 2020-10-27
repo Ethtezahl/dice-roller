fb2034ba07083881976d819476230d7302c1f3f3
Perlaki András <perlaki.andras@gmail.com>
2020-10-27 22:36:41 +0100

    added variant pattern handling to Group

--- src/Group.php
+++ src/Group.php
@@ -1,6 +1,7 @@
 <?php
 namespace Ethtezahl\DiceRoller;
 
+use InvalidArgumentException;
 use OutOfRangeException;
 
 final class Group
@@ -13,13 +14,19 @@ final class Group
     /**
      * @param int $pNumber Number of die
      * @param int $pSides Number of sides of each dice
+     * @param string $pVariant Type of variant calculation
      */
-    public function __construct(int $pNumber, int $pSides)
+    public function __construct(int $pNumber, int $pSides, string $pVariant = 'n')
     {
         $number = filter_var($pNumber, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]]);
+        $variantPattern = "/^[LnH]$/";
 
         if (!$number) {
             throw new OutOfRangeException('Invalid number of die.');
+        }
+
+        if (!preg_match($variantPattern, $pVariant)) {
+            throw new InvalidArgumentException('Variant roll indicator invalid.');
         }
 
         for ($i = 0; $i < $number; $i++) {
@@ -29,12 +36,13 @@ final class Group
 
     public function roll() : int
     {
-        $sum = 0;
+        $dieArray = array();
 
         foreach ($this->dice as $dice) {
-            $sum += $dice->roll();
+            array_push($dieArray, $dice->roll());
         }
-
+        
+        $sum = 0;
         return $sum;
     }
 }
